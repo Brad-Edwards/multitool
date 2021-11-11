ede9fe7884141a013402bcf98185edec5d853edb
Brad <56809295+Brad-Edwards@users.noreply.github.com>
2021-11-10 18:05:59 -0800

    Add project files.

---
+++ com.atomiklabs.multitool/Data/Generator.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+
+namespace com.atomiklabs.multitool.Data
+{
+    public class Generator
+    {
+        /// <summary>
+        /// The set of alphabetic characters.
+        /// </summary>
+        private const string _alphaChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        /// <summary>
+        /// The set of alpha-numeric characters.
+        /// </summary>
+        private const string _alphaNumericChars = _alphaChars + _numericChars;
+
+        /// <summary>
+        /// The set of numeric characters.
+        /// </summary>
+        private const string _numericChars = "1234567890";
+
+        /// <summary>
+        /// Generates cryptographically secure random bytes.
+        /// </summary>
+        /// <param name="size">Number of bytes required.</param>
+        /// <returns>An array with the requested number of random bytes.</returns>
+        public static byte[] GetRandomBytes(int size)
+        {
+            var rng = new RNGCryptoServiceProvider();
+            var bytes = new byte[size];
+            rng.GetBytes(bytes);
+            return bytes;
+        }
+
+        /// <summary>
+        /// Creates a random string.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetRandomString()
+        {
+            var random = new System.Random();
+            return new string(Enumerable.Repeat(_alphaNumericChars, 6).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ com.atomiklabs.multitool/Data/StringFormatter.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+
+
+namespace com.atomiklabs.multitool.Data
+{
+    /// <summary>
+    /// Represents various algorithms for formatting strings.
+    /// </summary>
+    public class StringFormatter
+    {
+       public string CapitalizeAll(string original)
+        {
+
+        }
+
+        public string CapitalizeEachFirst(string original)
+        {
+
+        }
+
+        public string CapitalizeFirst(string original)
+        {
+
+        }
+
+        public string CapitalizeAsTitle(string original)
+        {
+
+        }
+    }
+}
